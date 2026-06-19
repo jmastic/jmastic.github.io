52e5437d1ec0f1de84f0800b1003c3b42dfc780c
Justin Mastic <jmastic@gmail.com>
2026-06-18 22:31:29 -0600

    Update setlist

--- lyrics/setlist.js
+++ lyrics/setlist.js
@@ -1,85 +1,122 @@
-const setlist = [{
-      artist: "Incubus",
-      song: "Megalomaniac",
-      index: 00000000,
-    },{
-      artist: "Common Rider",
-      song: "Classics Of Love",
-      index: 00000001,
-    },{
-      artist: "Green Day",
-      song: "She",
-      index: 00000002,
-    },{
-      artist: "Black Sabbath",
-      song: "War Pigs",
-      index: 00000003,
-    },{
-      artist: "The Beatles",
-      song: "Happiness Is A Warm Gun",
-      index: 00000004,
-    },{
-      artist: "The Suicide Machines",
-      song: "Hey",
-      index: 00000005,
-    },{
-      artist: "System Of A Down",
-      song: "Aerials",
-      index: 00000006,
-    },{
-      artist: "BLACKPINK",
-      song: "Tally",
-      index: 00000007,
-    },{
-      artist: "Limp Bizkit",
-      song: "Break Stuff",
-      index: 00000008,
-    },{
-      artist: "Queens of the Stone Age",
-      song: "I Sat by the Ocean",
-      index: 00000009,
-    },{
-      artist: "Pink Floyd",
-      song: "Wish You Were Here",
-      index: 00000010,
-    },{
-      artist: "Muse",
-      song: "Knights of Cydonia",
-      index: 00000011,
-    },{
-      artist: "The Smashing Pumpkins",
-      song: "Bullet With Butterfly Wings",
-      index: 00000012,
-    },{
-      artist: "Refused",
-      song: "Summerholidays Vs. Punkroutine",
-      index: 00000013,
-    },{
-      artist: "Sap",
-      song: "Taste The Rain",
-      index: 00000014,
-    },{
-      artist: "Muse",
-      song: "Plug in Baby",
-      index: 00000015,
-    },{
-      artist: "Streetlight Manifesto",
-      song: "Failing, Flailing",
-      index: 00000016,
-    },{
-      artist: "Red Hot Chili Peppers",
-      song: "Carry Me Home",
-      index: 00000017,
-    },{
-      artist: "Soundgarden",
-      song: "Black Hole Sun",
-      index: 00000018,
-    },{
-      artist: "Nirvana",
-      song: "All Apologies",
-      index: 00000019,
-    },{
-      artist: "Arctic Monkeys",
-      song: "505",
-      index: 00000020,
-    },];
+const setlist = [
+  {
+    artist: "Incubus",
+    song: "Megalomaniac",
+    index: 00000000,
+  },
+  {
+    artist: "Common Rider",
+    song: "Classics Of Love",
+    index: 00000001,
+  },
+  {
+    artist: "Green Day",
+    song: "She",
+    index: 00000002,
+  },
+  {
+    artist: "Black Sabbath",
+    song: "War Pigs",
+    index: 00000003,
+  },
+  {
+    artist: "The Beatles",
+    song: "Happiness Is A Warm Gun",
+    index: 00000004,
+  },
+  {
+    artist: "The Suicide Machines",
+    song: "Hey",
+    index: 00000005,
+  },
+  {
+    artist: "System Of A Down",
+    song: "Aerials",
+    index: 00000006,
+  },
+  {
+    artist: "BLACKPINK",
+    song: "Tally",
+    index: 00000007,
+  },
+  {
+    artist: "Limp Bizkit",
+    song: "Break Stuff",
+    index: 00000008,
+  },
+  {
+    artist: "Queens of the Stone Age",
+    song: "I Sat by the Ocean",
+    index: 00000009,
+  },
+  {
+    artist: "Pink Floyd",
+    song: "Wish You Were Here",
+    index: 00000010,
+  },
+  {
+    artist: "Muse",
+    song: "Knights of Cydonia",
+    index: 00000011,
+  },
+  {
+    artist: "The Smashing Pumpkins",
+    song: "Bullet With Butterfly Wings",
+    index: 00000012,
+  },
+  {
+    artist: "Refused",
+    song: "Summerholidays Vs. Punkroutine",
+    index: 00000013,
+  },
+  {
+    artist: "Sap",
+    song: "Taste The Rain",
+    index: 00000014,
+  },
+  {
+    artist: "Muse",
+    song: "Plug in Baby",
+    index: 00000015,
+  },
+  {
+    artist: "Streetlight Manifesto",
+    song: "Failing, Flailing",
+    index: 00000016,
+  },
+  {
+    artist: "Red Hot Chili Peppers",
+    song: "Carry Me Home",
+    index: 00000017,
+  },
+  {
+    artist: "Soundgarden",
+    song: "Black Hole Sun",
+    index: 00000018,
+  },
+  {
+    artist: "Nirvana",
+    song: "All Apologies",
+    index: 00000019,
+  },
+  {
+    artist: "System Of A Down",
+    song: "Toxicity",
+    index: 00000020,
+  },
+  {
+    artist: "The Presidents of the United States of America",
+    song: "Kitty",
+    index: 00000021,
+  },
+  {
+    artist: "The Presidents of the United States of America",
+    song: "Lump",
+    index: 00000022,
+  },
+  {
+    artist: "Arctic Monkeys",
+    song: "A Certain Romance",
+    index: 00000023,
+  },
+];
